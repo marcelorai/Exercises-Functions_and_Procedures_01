81c623d31b3d39c6f14c32ecdfc97fa94e90f625
marcelorai <54368527+marcelorai@users.noreply.github.com>
2020-11-12 19:54:30 -0300

    Adicionando resposta 08

---
+++ respostas/questao_08.sql
@@ -0,0 +1,19 @@
+CREATE OR REPLACE FUNCTION public.quantas_atividades_projeto(cod_proj integer)
+RETURNS integer
+LANGUAGE 'plpgsql'
+AS $BODY$
+DECLARE
+total INTEGER;
+BEGIN
+select into total count(*)
+as total_atividades
+from projeto proj, atividade_projeto atv
+where atv.codprojeto=proj.codigo
+and proj.codigo=cod_proj;
+return total;
+END;
+$BODY$;
+
+select total_atividades_membro(proj.codigo)/quantas_atividades_projeto(proj.codigo)
+as porcentagem
+from projeto proj;
