c12155ef5bae90e43a55463f071dc2e0743e3e61
marcelorai <54368527+marcelorai@users.noreply.github.com>
2020-11-12 19:54:10 -0300

    Adicionando resposta 07

---
+++ respostas/questao_07.sql
@@ -0,0 +1,14 @@
+CREATE OR REPLACE FUNCTION total_atividades_membro(codProjeto INTEGER)
+RETURNS integer
+AS $$
+DECLARE
+total_atividades integer;
+BEGIN
+select into total_atividades count(*)
+from funcionario f, atividade_membro am
+where am.codmembro=f.codigo;
+return total_atividades;
+END;
+$$ LANGUAGE 'plpgsql';
+
+select total_atividades_membro('1');
